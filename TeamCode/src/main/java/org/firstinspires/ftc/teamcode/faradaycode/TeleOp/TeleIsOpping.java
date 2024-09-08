8b15828c330f04e12e727a50a964a367012d6773
pranavshankar111 <159638799+pranavshankar111@users.noreply.github.com>
2024-09-08 13:42:53 -0400

    added teleop.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/faradaycode/TeleOp/TeleIsOpping.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.faradaycode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.faradaycode.OpModes;
+
+@TeleOp(name="TeleIsOpping")
+public class TeleIsOpping extends OpModes {
+    
+}
